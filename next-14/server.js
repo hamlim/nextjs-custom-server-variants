50c38032ae06e83f1530c9bf49873428112c4365
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-11-21 10:41:51 -0500

    Use express for the custom server instead

--- next-14/server.js
+++ next-14/server.js
@@ -1,40 +1,54 @@
-const { createServer } = require('http')
+const express = require('express')
+const cookieParser = require('cookie-parser');
 const { parse } = require('url')
 const next = require('next')
+
+const app = express()
+
+app.use(cookieParser());
+app.use(express.json({limit: '1mb'}));
  
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = 3000
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+const nextApp = next({ dev, hostname, port })
+const handle = nextApp.getRequestHandler()
  
-app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
- 
-      if (pathname === '/') {
-        await app.render(req, res, '/', query)
-      } else if (pathname === '/legacy-page') {
-        await app.render(req, res, '/legacy-page', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
+nextApp.prepare().then(() => {
+
+  app.get('/', async (req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+
+    await nextApp.render(req, res, '/', query)
   })
-    .once('error', (err) => {
-      console.error(err)
-      process.exit(1)
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
+
+  app.get('/legacy-page', async (req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+
+    await nextApp.render(req, res, '/legacy-page', query)
+  })
+  
+  app.use(async (req, res) => {
+
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    // const { pathname, query } = parsedUrl
+
+
+    await handle(req, res, parsedUrl)
+  })
+
+  app.listen(port, () => {
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
 })
