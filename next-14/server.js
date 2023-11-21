ba972b1d894800dcb3081730507f4830d34cf233
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-11-21 11:58:47 -0500

    Working with local certs too

--- next-14/server.js
+++ next-14/server.js
@@ -10,45 +10,47 @@ app.use(express.json({limit: '1mb'}));
  
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
-const port = 3000
+const port = dev ? 443 : 3000
 // when using middleware `hostname` and `port` must be provided below
 const nextApp = next({ dev, hostname, port })
 const handle = nextApp.getRequestHandler()
  
 nextApp.prepare().then(() => {
-
-  app.get('/', async (req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
-
-
-    await nextApp.render(req, res, '/', query)
-  })
-
-  app.get('/legacy-page', async (req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
-
-
-    await nextApp.render(req, res, '/legacy-page', query)
-  })
   
   app.use(async (req, res) => {
-
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true)
-    // const { pathname, query } = parsedUrl
-
 
     await handle(req, res, parsedUrl)
   })
 
-  app.listen(port, () => {
-    console.log(`> Ready on http://${hostname}:${port}`)
-  })
+  if (process.env.NODE_ENV !== 'production') {
+    const https = require('https');
+    const fs = require('fs');
+    const homedir = require('os').homedir();
+    const path = require('path')
+  
+    server = https
+      .createServer(
+        {
+          key: fs.readFileSync(
+            path.join(homedir, '.local-certs', 'key.pem'),
+            'utf8'
+          ),
+          cert: fs.readFileSync(
+            path.join(homedir, '.local-certs', 'cert.pem'),
+            'utf8'
+          ),
+        },
+        app
+      )
+      .listen(port, () => {
+        console.log(`Listening at https://localhost:${port}/`);
+      });
+  } else {
+    server = app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}/`);
+    });
+  }
 })
