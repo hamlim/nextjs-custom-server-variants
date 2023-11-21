8bb595337af3c222d06f13970527d558e1a071ab
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-11-21 10:15:02 -0500

    Setup example apps

---
+++ next-13/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      Next 13
+    </main>
+  )
+}
---
+++ next-14/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      Next 14
+    </main>
+  )
+}
